acdd6acc2c0481ddf79d29a178646d3a84f33588
rolemadelen <rolemadelen17@pm.me>
2022-01-09 03:36:05 +0900

    Light/Dark Mode
    - tailwindcss 'dark:' used
    - light mode added
    - toggle icon added

--- pages/index.js
+++ pages/index.js
@@ -23,20 +23,20 @@ export default function Home({ allPostsData }){
       <section>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, subtitle, lang, date, title, tags }) => (
-            <li className={`${utilStyles.card} ${lang}`} key={id}>
+            <li className={`${utilStyles.card} ${lang} bg-card_bg dark:bg-dark_card_bg shadow-[0px_1px_3px_1px_#80afe7] dark:shadow-[0px_1px_3px_1px_black] duration-300`} key={id}>
               <Link href={`/posts/${id[0]}/${id[1]}`}>
-                  <a className="text-lg font-semibold">{title}</a>
+                  <a className="text-lg font-semibold text-primary dark:text-dark_primary">{title}</a>
               </Link>
               <br/>
-              <div className={`${utilStyles.subtext} text-sm pt-1 pb-2`}>
+              <div className={`text-sm pt-1 pb-2 text_secondary dark:text-dark_secondary`}>
                   {subtitle}
               </div>
-              <div className={`${utilStyles.subtext} text-xs`}>
+              <div className={`text-xs text_secondary dark:text-dark_secondary`}>
                   <Date dateString={date} />
               </div>
               <div className={`${utilStyles.tags} flex justify-end`}>
                 {tags.map((tag,i) => (
-                    <div key={i} className={"px-2 m-1 text-xs"}>
+                    <div key={i} className={"px-2 m-1 text-xs bg-tags_bg text-secondary dark:bg-dark_tags_bg dark:text-dark_secondary"}>
                         {tag}
                     </div>
                 ))}
