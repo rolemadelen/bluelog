bbd475870f28112d195cb937136602997466b720
Blue Eu <euisblue@pm.me>
2022-01-11 09:34:13 +0900

    utterances replaced with giscus

--- components/comments.js
+++ components/comments.js
@@ -6,10 +6,13 @@ const Comments = () => {
     const comment = useRef(null);
 
     const status = useScript({
-        url: "https://utteranc.es/client.js",
-        theme: "dark-blue",
-        issueTerm: "url",
+        url: "https://giscus.app/client.js",
         repo: "euisblue/nextjs-blog-comments",
+        repoId: "R_kgDOGp6ovg",
+        category: "General",
+        categoryId: "DIC_kwDOGp6ovs4CAoEv",
+        mapping: "url",
+        theme: "dark_dimmed",
         crossorigin: "anonymous",
         ref: comment
     });
