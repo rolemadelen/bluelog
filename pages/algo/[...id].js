d73d65620e5f9832321431c115a7b6c4f9fe3ceb
Blue Eu <euisblue@pm.me>
2022-01-15 01:17:14 +0900

    algo dark text header color modified

--- pages/algo/[...id].js
+++ pages/algo/[...id].js
@@ -10,8 +10,8 @@ export default function Algo({ post }) {
     }
     return (
         <Container customMeta={customMeta}>
-            <h1 className={`text-primary dark:text-dark_primary text-xl font-semibold mb-5`}>{post.title}</h1>
-            <article className={`text-primary dark:text-dark_primary pb-5 border-b-[1px] dark:border-gray-600`}>
+            <h1 className={`text-primary dark:text-dprimary text-xl font-semibold mb-5`}>{post.title}</h1>
+            <article className={`text-primary dark:text-dprimary pb-5 border-b-[1px] dark:border-gray-600`}>
                 <ReactMarkdown components={Codeblock}>{post.markdown}</ReactMarkdown>
             </article>
             <Comments />
