faa732ae0a5c5cb765bb4efaafa1813fc0506816
rolemadelen <rolemadelen17@pm.me>
2022-02-13 15:52:59 +0900

    tags conditional added

--- components/Post.js
+++ components/Post.js
@@ -13,13 +13,15 @@ const Post = ({ post, title, subtitle, date, children }) => {
             {date && (
                 <Date customClass={"text-xs"} dateString={date} />
             )}
-            <div className={`${utilStyles.tags} flex justify-end`}>
-                {post.tags.map((tag, i) => (
-                    <div key={i} className={"px-2 mr-1 text-xs border-[1.5px] border-tags_bg text-secondary dark:border-dtags_bg dark:text-dsecondary"}>
-                        {tag}
-                    </div>
-                ))}
-            </div>
+            {tags && (
+                <div className={`${utilStyles.tags} flex justify-end`}>
+                    {post.tags.map((tag, i) => (
+                        <div key={i} className={"px-2 mr-1 text-xs border-[1.5px] border-tags_bg text-secondary dark:border-dtags_bg dark:text-dsecondary"}>
+                            {tag}
+                        </div>
+                    ))}
+                </div>
+            )}
             {post && (
                 <LanguageButtons className={`postTag`}>
                     {post.availableLanguage.map((post) => (
