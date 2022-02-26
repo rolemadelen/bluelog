a70a2fb955e0974544e88295ff60debec7c6e9bf
Blue Eu <euisblue@pm.me>
2022-02-27 07:59:40 +0900

    Animoji added

--- layouts/home.js
+++ layouts/home.js
@@ -8,21 +8,10 @@ import utilStyles from '@styles/utils.module.scss'
 import React, { useState } from 'react'
 
 const HomeLayout = ({ blog }) => {
-    const [profileImage, setProfileImage] = useState("/images/blue.png");
-
-    function genProfileImage() {
-        const index = Math.floor(Math.random() * 3 + 1);
-        setProfileImage(`/images/blue${index}.svg`);
-    }
-
-    React.useEffect(() => {
-        genProfileImage();
-    }, []);
-
     return (
         <Container>
             <Banner>
-                <Image className={utilStyles.circleImage} width={170} height={170} src={profileImage} alt="Blue Eu" />
+                <Image className={utilStyles.circleImage} width={135} height={135} src={"/images/animoji2.gif"} alt="Blue Eu" />
                 <PageBanner
                     title={"Failure leads to understanding."}
                     subtitle={"If you're a living organism, failure in life is inevitable. \nWe fail by default. So why not we learn from it?"}
